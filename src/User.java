adb251c1df4c1fa752ba92f5d2ee5130e119fe39
kowsar <kowsar.sh2000@gail.com>
2019-06-21 17:44:32 +0430

    new class added to model a friend (a client) in network

--- src/User.java
+++ src/User.java
@@ -7,6 +7,64 @@ public class User {
     private int Password;
     private ArrayList<User> friends;
     private PlayList playList=new PlayList("Shared PlayList");
+    private Long time;
+    private Music music;
+    boolean playing=false;
+
+    public PlayList getPlayList() {
+        return playList;
+    }
+
+    public void setPlayList(PlayList playList) {
+        this.playList = playList;
+    }
+
+    public Long getTime() {
+        return System.currentTimeMillis();
+    }
+
+    public String Difference(User user){
+        long userTime=user.getTime();
+        long dif=System.currentTimeMillis()-userTime;
+        int diff= (int) (dif/10000);
+        if(dif <=user.getMusic().getTime()*1000){
+            playing=true;
+            return "Playing...";
+        }
+
+        playing=false;
+        int day=(int) (diff
+                / 86400);
+        int hour= (int) (dif/3600000);
+        int min= (int) (dif/60000);
+        int sec= (int) (dif/1000);
+
+        if(day==0){
+            if(hour==0){
+                if (min==0){
+                    return ""+sec;
+                }
+                else return ""+min;
+            }
+            else return ""+hour;
+
+        }
+        else return ""+day;
+
+
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
 
     public User(String name) {
         this.name = name;
@@ -57,5 +115,27 @@ public class User {
     }
     public void addMusic(Music music){
         this.playList.addSong(music);
+    }
+
+    public String giveActivity(User user){
+        if (this.friends.contains(user)){
+            if (user.getPlayList().getSongs().contains(user.getMusic())){
+                if (user.isPlaying())
+                return user.getName()+"\n"+"Playing..."+"\n"+user.getMusic().getTitle();
+
+                else return user.getName()+"\n"+Difference(user)+" ago" +"\n"+user.getMusic().getTitle();
+            }
+        }
+
+
+        return null;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    public void setPlaying(boolean playing) {
+        this.playing = playing;
     }
 }
