4ebcae6c2b7d9ac12de1766766fa16e3d0fac4a5
9731059 <9731059@ceit.local>
2019-06-26 11:32:23 +0430

    adding client and server

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,39 @@
 import java.io.*;
-import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Client {
 
+    public static void transferFromClientToServer1(String addressFile) throws IOException{
+        int port = 15064;
+        Socket sr = new Socket("localhost",port);
+        FileInputStream fr = new FileInputStream(addressFile);
+        OutputStream out = sr.getOutputStream();
+
+        byte[] buffer = new byte[64 * 1024];
+        int bytesRead = 0;
+        long totalSent = 0;
+
+        while ((bytesRead = fr.read(buffer)) != -1) {
+            if (bytesRead > 0) {
+                out.write(buffer, 0, bytesRead);
+                totalSent += bytesRead;
+                System.out.println("sent " + totalSent);
+            }
+        }
+
+        sr.close();
+
+        /*
+        byte[] b = new byte[9999999];
+        fr.read(b,0,b.length);
+        OutputStream os = sr.getOutputStream();
+        os.write(b,0,b.length);
+*/}
 
 
-    public static void transferfromserver(String fileName) throws IOException {
-        Socket sock = new Socket("172.24.100.85", 15064);
+    public static void transferfromserverToClient1(String fileName) throws IOException {
+        Socket sock = new Socket("localhost", 15064);
         InputStream in = sock.getInputStream();
         FileOutputStream fileOutputStream = new FileOutputStream(fileName);
 
@@ -20,77 +46,23 @@ public class Client {
         fileOutputStream.close();
     }
 
+    public static void main(String[] args) {
+       /*
+        try {
+            System.out.println("9(((((((((((((((((((((((((((((((((");
+            transferfromserverToClient1("tahoora.mp3");
+            System.out.println("10((((((((((((((((((((((((((((((((((");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
 
-
-    public void transfertoserver(File file){}
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) throws IOException {
-        LoginPage login = new LoginPage();
-        PlayList list = HomePage.user.getSharedPlayList();
-
-
-
-
-        Socket sendName = new Socket("172.24.100.85", 12345);
-        Socket sendIP = new Socket("172.24.100.85", 12346);
-        OutputStream osname = sendName.getOutputStream();
-        OutputStreamWriter oswname = new OutputStreamWriter(osname);
-        BufferedWriter bwname = new BufferedWriter(oswname);
-        bwname.write(HomePage.user.getName());
-        bwname.flush();
-
-
-        InputStream isname = sendName.getInputStream();
-        InputStreamReader isrname = new InputStreamReader(isname);
-        BufferedReader brname = new BufferedReader(isrname);
-        String message = brname.readLine();
-        if (message.equals("ok")) {
-            sendName.close();
+        try {
+            transferFromClientToServer1("0.bin");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-
-        OutputStream osIP = sendName.getOutputStream();
-        OutputStreamWriter oswIP = new OutputStreamWriter(osIP);
-        BufferedWriter bwIP = new BufferedWriter(oswIP);
-        bwname.write(InetAddress.getLocalHost().toString());
-        bwname.flush();
-
-        InputStream isIP = sendIP.getInputStream();
-        InputStreamReader isrIP = new InputStreamReader(isIP);
-        BufferedReader brIP = new BufferedReader(isrIP);
-        String IP = brIP.readLine();
-        if (IP.equals("ok")) {
-            sendIP.close();
-        }
-    while(true){
-        if (HomePage.flag) {
-
-//            transfertoserver();
-            HomePage.flag = false;
-
-        }
-        if (HomePage.flag2){
-
-            transferfromserver(HomePage.needed);
-            HomePage.flag2=false;
-
-        }
-
-        }
-
-
-
-
-            }
     }
 
-
+    }
--- src/Server.java
+++ src/Server.java
@@ -1,37 +1,47 @@
-import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.UnsupportedTagException;
-import javazoom.jl.decoder.JavaLayerException;
-
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class Server {
-     static Socket acceptName;
-    static Socket acceptIP;
-    static Socket acceptFile;
-   static ArrayList<File> DataBase;
-    public static ArrayList<User> users;
-   
-    public static void transfertoserver() throws IOException {
-        ServerSocket servsock = new ServerSocket(15065);
 
 
 
 
+    public static void transferFromClientToServer(String fileName) throws IOException {
+        // ServerSocket servsock = new ServerSocket(15065);
 
+        int port = 15064;
+        byte[] b = new byte[9999999];
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server Started and listening to the port 25000");
+        Socket sock = serverSocket.accept();
+        InputStream is = sock.getInputStream();
+        //address to save
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        byte [] buffer = new byte[64*1024];
+        int bytesRead = 0;
+
+        while ( (bytesRead = is.read(buffer)) != -1)
+            fileOutputStream.write(buffer, 0, bytesRead);
+        sock.close();
+        fileOutputStream.close();
+
+            /*
+
+             FileOutputStream fr = new FileOutputStream(fileName);
+             is.read(b,0,b.length);
+             fr.write(b,0,b.length);
+             */
 
 
     }
-    public static void transferfromserver(File file) throws IOException {
+    public static void transferfromserverToClient(String name) throws IOException {
         ServerSocket servsock = new ServerSocket(15064);
         Socket sock = servsock.accept();
         long time = System.currentTimeMillis();
 
         OutputStream out = sock.getOutputStream();
-        FileInputStream fileInputStream = new FileInputStream("1234.mp4");
+        FileInputStream fileInputStream = new FileInputStream(name);
 
         byte[] buffer = new byte[64 * 1024];
         int bytesRead = 0;
@@ -51,63 +61,24 @@ public class Server {
                 + (System.currentTimeMillis() - time) + "ms.");
     }
 
-   
-    static ArrayList<String> IPs=new ArrayList<>();
-    public static ArrayList<String> usernames=new ArrayList<>();
- 
-    public ArrayList<String> getAllUserNames(){
-        return usernames;
-    }
-
-    public static void main(String[] args) throws IOException, InvalidDataException, JavaLayerException, UnsupportedTagException {
-        LoginPage login = new LoginPage();
-        login.setVisible(true);
-        IPs.add(InetAddress.getLocalHost().getHostAddress().toString());
-        usernames.add(HomePage.user.getName());
-        ServerSocket sSocketIP = new ServerSocket();
-        
-        while (true) {
-            acceptIP = sSocketIP.accept();
-//            acceptName = sSocketUserName.accept();
-            BufferedReader clientName = new BufferedReader(new InputStreamReader(acceptName.getInputStream()));
-
-            DataOutputStream outToClientname = new DataOutputStream(acceptName.getOutputStream());
-
-            String name = clientName.readLine();
-            usernames.add(name);
-            outToClientname.writeBytes("ok");
-
-
-            BufferedReader clientIP = new BufferedReader(new InputStreamReader(acceptIP.getInputStream()));
-
-            DataOutputStream outToClientIP = new DataOutputStream(acceptIP.getOutputStream());
-
-            String IP = clientIP.readLine();
-            IPs.add(name);
-            outToClientIP.writeBytes("ok");
-
-
-//            acceptFile = sSocketFile.accept();
-            BufferedReader clientFile = new BufferedReader(new InputStreamReader(acceptFile.getInputStream()));
-
-            DataOutputStream outToClientFile = new DataOutputStream(acceptFile.getOutputStream());
-
-            String FileName = clientName.readLine();
-            File file = new File(FileName);
-
-            for (File file1 : DataBase) {
-                if ((new Music(file1)).getTitle().equals(FileName)) {
-                    file = file1;
-                    break;
-                }
-            }
-
-            transferfromserver(file);
-
-            outToClientname.writeBytes("ok");
+    public static void main(String[] args) {
+       /*
+        try {
+            System.out.println("9)))))))))))))))))))))))))))))");
+            transferfromserverToClient("123.mp3");
+            System.out.println("10)))))))))))))))))))))))))))))))))");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+        try {
+            transferFromClientToServer("yas.bin");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-    }
 
     }
 
+
+}
